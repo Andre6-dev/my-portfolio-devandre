715018b628b0b72982966c0dac33123cd2bc471a
Andre Gallegos <andre.gallegos@tecsup.edu.pe>
2022-09-03 00:29:40 -0500

    Email, Footer, scroll and links
    All working, button to scroll up, links to different parts in the page and email sending works.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,16 @@ import React, { useState } from 'react'
 import "./header.css";
 
 const Header = () => {
+  /*=================== Change Background Header =======================*/
+  window.addEventListener("scroll",  function () {
+    const header = this.document.querySelector(".header");
+    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+  })
+
   /*=================== Toggle Menu =======================*/
   const [Toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
   return (
     <header className="header">
       <nav className="nav container">
@@ -12,22 +20,30 @@ const Header = () => {
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
+              <a href="#home"
+                 onClick={() => setActiveNav('#home')}
+                 className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>
             </li>
             <li className="nav__item">
-              <a href="#about" className="nav__link">
+              <a href="#about"
+                 onClick={() => setActiveNav('#home')}
+                 className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
                 <i className="uil uil-user nav__icon"></i> About
               </a>
             </li>
             <li className="nav__item">
-              <a href="#skills" className="nav__link">
+              <a href="#skills"
+                 onClick={() => setActiveNav('#skills')}
+                 className={activeNav === "#skills" ? "nav__link active-link" : "nav__link"}>
                 <i className="uil uil-file-alt nav__icon"></i> Skills
               </a>
             </li>
             <li className="nav__item">
-              <a href="#services" className="nav__link">
+              <a href="#services"
+                 onClick={() => setActiveNav('#services')}
+                 className={activeNav === "#services" ? "nav__link active-link" : "nav__link"}>
                 <i className="uil uil-briefcase-alt nav__icon"></i> Services
               </a>
             </li>
@@ -37,7 +53,9 @@ const Header = () => {
               </a>
             </li>
             <li className="nav__item">
-              <a href="#contact" className="nav__link">
+              <a href="#contact"
+                 onClick={() => setActiveNav('#contact')}
+                 className={activeNav === "#contact" ? "nav__link active-link" : "nav__link"}>
                 <i className="uil uil-message nav__icon"></i> Contact
               </a>
             </li>
